9658697c40f8821a9721b0113d8221529ab6a321
Liferoge <74018033+Liferoge@users.noreply.github.com>
2026-07-15 17:05:43 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
+import express from "express";
 
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send(`
+        <h1>AMQSync Server</h1>
+        <p>Servidor online.</p>
+    `);
+});
+
+app.listen(PORT, () => {
+    console.log(`AMQSync iniciado na porta ${PORT}`);
+});
